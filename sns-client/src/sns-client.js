b53b4b0e5b4160f0c4117d3e735733480d46cdfa
Connor Harness <connor@kontrasecurities.com>
2018-10-16 19:15:41 -0600

    Corrected sns message format

--- sns-client/src/sns-client.js
+++ sns-client/src/sns-client.js
@@ -6,19 +6,24 @@ class Sns {
     this.sns = new Aws.SNS();
   }
 
-  async publishToTopic({ topicName, payload }) {
+  async publishToTopic({ topicName, title, body }) {
     const topic = await this.sns.createTopic({ Name: topicName }).promise();
+
+    const msgStr = JSON.stringify({
+      default: 'This is the default message. Your platform is not supported yet.',
+      GCM: JSON.stringify({
+        notification: {
+          title,
+          body
+        },
+        priority: 'high'
+      })
+    });
 
     const params = {
       TopicArn: topic.TopicArn,
       MessageStructure: 'json',
-      Message: {
-        default: 'This is the default message. Your platform is not supported yet.',
-        GCM: {
-          data: payload,
-          priority: 'high'
-        }
-      },
+      Message: msgStr,
     };
 
     return this.sns.publish(params).promise();
